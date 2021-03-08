715c5c386e1b8f7bc8e486be4aca1546ec4624ea
tom-villanueva <54720888+tom-villanueva@users.noreply.github.com>
2021-03-08 17:25:39 -0300

    Update Dashboard

--- frontendreact/src/components/dashboard/Dashboard.tsx
+++ frontendreact/src/components/dashboard/Dashboard.tsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import { RouteComponentProps } from "react-router-dom";
 import ItemList from '../items/ItemList'
+import ItemService from "../../services/ItemService";
 
 export default class Dashboard extends Component<RouteComponentProps> {
+
+    state = {
+        Items: []
+    }
+
+    async componentDidMount() {
+        const response = await ItemService.getProjectItems(this.props.match.params.id.project_id);
+        const items = response.data;
+    }
+
     render() {
         return (
             <div>
